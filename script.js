a39003d954d2e4d97dcba570606e6c6d14a88d48
DAJ350 <daniellajorin@gmail.com>
2024-10-18 16:26:07 +0100

    error class addition

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+/*
+
+ Create a function that turns the form border-bottom colour to red if the user's 
+ input is not valid on submission attempt.
+
+ On the submission of the form, the function will need to check all the input values of
+ the form to find any inputs that are invalid. 
+
+ For any inputs that return invalid, it should display an error message underneath the 
+ input field and the red exclamation mark icon within the input field.
+
+ Inputs: 
+ - submission button click event.
+ - submitted values of all input elements.
+
+ Output:
+ - If value of any input field is invalid upon submission, 
+it should display the error message of that input's parent
+container. 
+ */
+
